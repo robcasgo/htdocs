fea427fe82afd0d7b816adaecb046f74f471fede
pc_pabloserrano <robcasgo@gmail.com>
2023-11-09 13:43:10 +0100

    1

--- scrapyard/login.php
+++ scrapyard/login.php
@@ -1,18 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Scrapyard</title>
+    <title>Document</title>
 </head>
- 
 <body>
-<h1>UNCLE JOE'S SCRAPYARD</h1>
-<!-- boton al login -->
-<a href="login.php">Identificate</a>
-
-
+<!-- https://code.tutsplus.com/es/create-a-php-login-form--cms-33261t -->    
+    <h1>INICIA SESION</h1>
 </body>
-
 </html>
