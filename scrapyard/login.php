2b2269127394a4723e63ee5c60a8f3e1b928314a
pc_pabloserrano <robcasgo@gmail.com>
2023-11-09 14:00:01 +0100

    2

--- scrapyard/login.php
+++ scrapyard/login.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<!-- https://code.tutsplus.com/es/create-a-php-login-form--cms-33261t -->    
+    <!-- 
+    https://code.tutsplus.com/es/create-a-php-login-form--cms-33261t 
+    https://parzibyte.me/blog/2019/01/22/ejemplo-simple-login-php/
+    -->
     <h1>INICIA SESION</h1>
+    <form action="validarlogin.php" method="post">
+        <input name="usuario" type="text" placeholder="Usuario">
+        </br>
+        <input name="contraseña" type="password" placeholder="Contraseña">
+        </br>
+        <input type="submit" value="Iniciar sesión">
+    </form>
 </body>
+
 </html>
